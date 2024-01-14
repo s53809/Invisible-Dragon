05e28b4c2c09e86dd1ec66fe68b4f6826bf1b39b
Minsoda <dg4res@gmail.com>
2024-01-14 23:56:41 +0900

    Implementing some InGame Logic

---
+++ InvisibleDragon/Assets/Scripts/MainGame/BeatCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BeatCalculator
+{
+    private StagePattern m_inputedPattern;
+    public BeatCalculator(StagePattern inputedPattern)
+    {
+        m_inputedPattern = inputedPattern;
+    }
+
+    public void Update()
+    {
+
+    }
+}
---
+++ InvisibleDragon/Assets/Scripts/MainGame/StagePattern.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StagePattern
+{
+    public Int32 stageNum;
+    public String stageName;
+    public List<Tuple<Int32, Int32>> perBeat;
+    public List<String> touchBeat;
+}
